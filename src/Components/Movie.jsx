0aadad56c67c4792dde0affa53c98d336a22d127
StarSimo00 <sasimo2003@gmail.com>
2022-11-01 23:49:40 +0100

    Here you write message while committing, you can write anything here

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -1,12 +1,12 @@
 const Movie = ({data}) => {
     return ( 
-        <div className="container col-6 g-2 " key={data.id}>
+        <div className="container col-md-6 g-2 " key={data.id}>
             <div className="card text-bg-dark">
-                <img src={'http://image.tmdb.org/t/p/original'+ data.backdrop_path } className="card-img zoom-in" />
+                <img src={'http://image.tmdb.org/t/p/original'+ data.backdrop_path } className="card-img zoom-in" alt="g"/>
             <div className="card-img-overlay p-4 test">
                 <h5 className="card-title"> {data.title} </h5>
-                <p className="card-text ms-2"> {data.overview} </p>
-                <p className="card-text ms-1"><small> <i class="bi bi-star-fill"></i> {data.vote_average}</small></p>
+                <p className="card-text ms-2 none"> {data.overview} </p>
+                <p className="card-text ms-1"><small> <i className="bi bi-star-fill"></i> {data.vote_average}</small></p>
             </div>
             </div>
 
