fd391feb29f552508947bb702415857b02b16430
StarSimo00 <sasimo2003@gmail.com>
2022-11-05 22:33:19 +0100

    New Changes

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -26,22 +26,22 @@ const Movie = ({data}) => {
 
 
     return ( 
-        <div className=" container p-2 animate__animated animate__fadeIn container col-md-4 g-1 onhover" key={data.id}>
+        <div className=" container p-2 animate__animated animate__fadeIn container col-lg-3 col-md-6 col-sm-12 g-1 onhover" key={data.id}>
             <div className="card text-bg-dark">
-                <img src={'http://image.tmdb.org/t/p/original'+ data.backdrop_path } className="card-img zoom-in" alt="g"/>
+                <img src={'http://image.tmdb.org/t/p/original'+ data.poster_path } className="card-img zoom-in" alt="g"/>
             <div className="card-img-overlay p-4 test">
                 <h5 className="card-title"> {data.title} </h5>
                 <span className="card-text ms-1"><small> Rate : <i className="bi bi-star-fill"></i> {data.vote_average}</small></span>
                 <span className="card-text ms-1 d-block my-1"><small>{data.release_date}</small></span>
                 { Current_Location.pathname === '/Profile' ?  
                  <div>
-                     <button onClick={ () => handleDelete(data.id) } className='btn btn-outline-danger'  > DELETE </button>
-                     <Link to={`/Movie/${data.id}`} state={{ Movie : data }} ><button className="btn btn-outline-secondary mx-1 "> Details </button></Link>
+                     <button onClick={ () => handleDelete(data.id) } className='btn btn-outline-danger mx-1 my-1'  > DELETE </button>
+                     <Link to={`/Movie/${data.id}`} state={{ Movie : data }} ><button className="btn btn-outline-secondary mx-1 my-1 "> Details </button></Link>
                  </div>
                 : 
                 <div>
-                    <button onClick={ handleClick } className={ isActive ? 'btn btn-outline-secondary disabled'  :  'btn btn-outline-secondary'  } > <i className="bi bi-star-fill"></i> Add To Fav !</button>
-                    <Link to={`/Movie/${data.id}`} state={{ Movie : data }} ><button className="btn btn-outline-secondary mx-1 "> Details </button></Link>
+                    <button onClick={ handleClick } className={ isActive ? 'btn btn-outline-secondary disabled mx-1 my-1 '  :  'btn btn-outline-secondary mx-1 my-1'  } > <i className="bi bi-star-fill"></i> Add To Fav !</button>
+                    <Link to={`/Movie/${data.id}`} state={{ Movie : data }} ><button className="btn btn-outline-secondary mx-1 my-1 "> Details </button></Link>
 
                 </div>
 
--- src/Components/MovieDetails.jsx
+++ src/Components/MovieDetails.jsx
@@ -5,17 +5,18 @@ const MovieDetails = () => {
     const { Movie } = location.state
 
     console.log(Movie);
+
+    let imgUrl = 'url( http://image.tmdb.org/t/p/original' + Movie.backdrop_path  + ')'
+
     return ( 
-        <div className="animate__animated animate__fadeIn">
-        
-        <div class="card text-bg-dark">
-            <img src={'http://image.tmdb.org/t/p/original'+ Movie.backdrop_path } height={541} className='detail' alt={`Movie Img ${Movie.title}`} />
-            <div class="card-img-overlay m-5 p-5">
-                <h5 class="card-title">{Movie.title}</h5>
-                <p class="card-text">{Movie.overview}</p>
-                <p class="card-text"><small>{Movie.release_date}</small></p>
+        <div className="animate__animated animate__fadeIn detail p-5 ">
+            <div className="card-img-overlay m-5 p-5 text-white full" style={ {  backgroundImage : imgUrl  } } >
+                <div className="card-img-overlay  p-5 tests">
+                    <h5 className="card-title">{Movie.title}</h5>
+                    <p className="card-text">{Movie.overview}</p>
+                    <p className="card-text"><small>{Movie.release_date}</small></p>
+                </div>
             </div>
-        </div>
         </div>
      );
 }
